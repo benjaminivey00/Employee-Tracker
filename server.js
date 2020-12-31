49d959d53af4acc82cafa961a93ba0648e74dcce
Ben Ivey <benjaminivey00@gmail.com>
2020-12-31 10:09:26 -0700

    first questions and fixes

--- server.js
+++ server.js
@@ -14,3 +14,40 @@ var connection = mysql.createConnection(
         password: "Bi9602689",
         database: "employee_DB"
     });
+connection.connect(function(err){
+    if (err) throw err;
+    chooseRole();
+});
+
+    function chooseRole(){
+        inquirer.prompt({
+
+            name: 'intitialQuestions',
+            type: "list",
+            message: "Please select what you would like to do.",
+            choices: [
+                "Add departments, roles, employees",
+                "View departments, roles, employees",
+                "Update employee roles",
+                "Exit"
+            ]
+        }).then((answer) =>{
+            switch (answer.intitialQuestions) {
+                case "Add departments, roles, employees":
+                    addFunc();
+                    break;
+
+                    case "View departments, roles, employees":
+                        viewFunc();
+                        break;
+
+                    case "Update employee roles":
+                        updateFunc();
+                        break;
+
+                    case "Exit":
+                     console.log("Connection ended.")
+                     connection.end();
+            }
+        });
+    };

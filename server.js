64dd6c179a80df8ad83922d1dec6c56a27092069
Ben Ivey <benjaminivey00@gmail.com>
2021-01-04 20:21:47 -0700

    finished functionality

--- server.js
+++ server.js
@@ -114,3 +114,216 @@ connection.connect(function(err){
         });
     
     };
+
+    function viewFunc() {
+        inquirer   
+            .prompt({
+                name: "viewWhich",
+                type: "list",
+                message: "Select what you would like to view",
+                choices: [
+                    "Departments",
+                    "Roles",
+                    "Employees"
+                ]
+            })
+            .then((answer) => {
+                switch (answer.viewWhich) {
+                    case "Departments":
+                        viewDepartmentFunc();
+                        break;
+    
+                    case "Roles":
+                        viewRolesFunc();
+                        break;
+    
+                    case "Employees":
+                        viewEmployeesFunc()
+                        break;
+                };
+            });
+    };
+    function viewDepartmentFunc() {
+        connection.query("SELECT * FROM department", function(err, res) {
+            if (err) throw err;
+            if (res === []) {
+                console.log("There are no departments to view.")
+            } else {
+                console.table(res);
+                chooseRole();
+            };
+        });
+    };
+    function viewRolesFunc() {
+        connection.query("SELECT * FROM role", function(err, res) {
+            if (err) throw err;
+            if (res === []) {
+                console.log("There are no roles to view.")
+            } else {
+                console.table(res);
+                chooseRole();
+            };
+        });
+    };
+    function viewEmployeesFunc() {
+        connection.query("SELECT * FROM employee", function(err, res) {
+            if (err) throw err;
+            if (res === []) {
+                console.log("There are no employees to view.")
+            } else {
+                console.table(res);
+                chooseRole();
+            };
+        });
+    };
+    // function that adds departments roles and employees to mysql db
+    function addFunc() {
+        inquirer
+            .prompt({
+                name: "departmentRoleEmployee",
+                type: "list",
+                message: "Select what you would like to add.",
+                choices: [
+                    "Department",
+                    "Role",
+                    "Employee"
+                ]
+            })
+            .then((answer) => {
+                switch (answer.departmentRoleEmployee) {
+                    case "Department":
+                        addDepartmentFunc();
+                        break;
+    
+                    case "Role":
+                        addRoleFunc();
+                        break;
+    
+                    case "Employee":
+                        addEmployeeFunc();
+                        break;
+                }
+            });
+    };
+    
+    function addEmployeeFunc() {
+        connection.query(
+            "SELECT * FROM role",
+            function(err, res) {
+                let roleArray = []
+                for(let i = 0; i < res.length; i++) {
+                    roleArray.push(res[i].title)
+                }
+                if (err) throw err;
+                inquirer
+                    .prompt([
+                        {
+                            name: "whichrole",
+                            type: "list",
+                            message: "Which role would you like to add an employee to?",
+                            choices: roleArray
+                        },
+                        {
+                            name: "employeeFirstName",
+                            type: "input",
+                            message: "What is the first name of the employee?"
+                        },
+                        {
+                            name: "employeeLastName",
+                            type: "input",
+                            message: "What is the last name of the employee?"
+                        }
+                        
+                    ])
+                    .then((response) => {
+                        let roleID;
+                        let hasManager = null;
+                        
+                        for (let i = 0; i < roleArray.length; i++) {
+                            if (res[i].title === response.whichrole) {
+                                roleID = res[i].id
+                            };
+                        };
+                        connection.query("INSERT INTO employee SET ?",
+                        {
+                            id: response.id,
+                            first_name: response.employeeFirstName + "",
+                            last_name: response.employeeLastName + "",
+                            role_id: roleID,
+                            manager_id: hasManager
+                        })
+                        chooseRole();
+                    });
+            }
+        );
+    }
+    function addRoleFunc() {
+        connection.query(
+            "SELECT * FROM department",
+            function(err, res) {
+                if (err) throw err;
+                inquirer
+                    .prompt([
+                        {
+                        name: "whichdepartment",
+                        type: "list",
+                         message: "Select which department you would like to add a role to.",
+                        choices: res
+                        },
+                        {
+                        name: "title",
+                        type: "input",
+                        message: "Please input the role you would like to add."
+                        },
+                        {
+                        name: "salary",
+                        type: "input",
+                        message: "Input the persons salary."
+                        }
+                    ])
+                    .then((result) => {
+                        let departmentID;
+                        for (let i = 0; i < res.length; i++) {
+                            if (res[i].name === result.whichdepartment) {
+                                departmentID = res[i].id
+                            }
+                        };
+                        connection.query(
+                            "INSERT INTO role SET ?",
+                            {
+                                id: result.id,
+                                title: result.title + "",
+                                salary: result.salary,
+                                department_id: departmentID
+                            },
+                            function(err, res) {
+                                if (err) throw err
+                            }
+                        );
+                        chooseRole();
+                    });
+                });
+            
+    }
+    
+    function addDepartmentFunc() {
+        inquirer
+            .prompt({
+                name: "department",
+                type: "input",
+                message: "Please input the department you would like to add."
+            })
+            .then((res) => {
+                connection.query(
+                    "INSERT INTO department SET ?",
+                    {
+                        id: res.id,
+                        name: res.department + ""
+                    },
+                    function(err, res) {
+                        if (err) throw err
+                    }
+                );
+                chooseRole();
+            });
+    };
